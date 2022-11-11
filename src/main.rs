efaf7fcf6d05dd03257d75de77fcd3d12be19bca
Eric Correia <correia.eh@gmail.com>
2022-11-11 09:39:52 -0500

    use connection pool instead of connection

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sqlx::{Connection, PgConnection};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 use zero2prod::{configuration::get_configuration, startup::run};
@@ -7,7 +7,7 @@ use zero2prod::{configuration::get_configuration, startup::run};
 async fn main() -> std::io::Result<()> {
     // panic if we can't read the conf
     let config = get_configuration().expect("Failed to read config");
-    let connection = PgConnection::connect(&config.database.connection_string())
+    let connection = PgPool::connect(&config.database.connection_string())
         .await
         .expect("Failed to connect to Postgres");
     let address = format!("127.0.0.1:{}", config.application_port);
