af8a76e1472ca98b2acfb94884ab786b442d32d5
Eric Correia <correia.eh@gmail.com>
2022-11-11 08:32:33 -0500

    set up postgres connection and pass it into our app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 
 use zero2prod::{configuration::get_configuration, startup::run};
@@ -6,7 +7,10 @@ use zero2prod::{configuration::get_configuration, startup::run};
 async fn main() -> std::io::Result<()> {
     // panic if we can't read the conf
     let config = get_configuration().expect("Failed to read config");
+    let connection = PgConnection::connect(&config.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
     let address = format!("127.0.0.1:{}", config.application_port);
     let listener = TcpListener::bind(address).expect("Failed to bind to address");
-    run(listener)?.await
+    run(listener, connection)?.await
 }
