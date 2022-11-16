b37e044ea338cd4d9d3b89e39aaeb0dee3df366f
Eric Correia <correia.eh@gmail.com>
2022-11-16 09:01:32 -0500

    move functions for setting up telemetry into its own module

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,17 @@
 use sqlx::PgPool;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 use std::net::TcpListener;
 
-use zero2prod::{configuration::get_configuration, startup::run};
+use zero2prod::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Redirect all `log`'s events to our subscriber
-    LogTracer::init().expect("Failed to set logger");
-
-    // fall back to printing all spans at info-level or above
-    // if the RUST_LOG environment variable has not been set.
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new(
-        "zero2prod".into(),
-        // Output the formatted spans to stdout
-        std::io::stdout,
-    );
-    // The `with` method is provided by `SubscriberExt`, an extension
-    // trait fir `Subscriber` exposed by `tracing_subscriber`
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    // `set_global_default` can be used by applications to specify
-    // what subscriber should be used to process spans.
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
 
     // panic if we can't read the conf
     let config = get_configuration().expect("Failed to read config");
