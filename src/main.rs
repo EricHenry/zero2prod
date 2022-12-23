540441ed55259409a80adaa95c0f552d3398c152
Eric Correia <correia.eh@gmail.com>
2022-12-23 12:05:31 -0500

    set up app for deploying to digital ocean

--- src/main.rs
+++ src/main.rs
@@ -20,8 +20,7 @@ async fn main() -> std::io::Result<()> {
     // pool is used for the first time
     let connection = PgPoolOptions::new()
         .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy(config.database.connection_string().expose_secret())
-        .expect("Failed to connect to Postgres");
+        .connect_lazy_with(config.database.with_db());
     let address = format!("{}:{}", config.application.host, config.application.port);
     let listener = TcpListener::bind(address).expect("Failed to bind to address");
     run(listener, connection)?.await
