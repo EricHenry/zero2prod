b3976807608374170e9c9d6d4fc44474d5636695
Eric Correia <correia.eh@gmail.com>
2023-01-22 22:33:20 -0500

    create an application struct to hold onto application intialization logic to reduce duplication across testing and running th application

--- src/startup.rs
+++ src/startup.rs
@@ -1,13 +1,62 @@
 use actix_web::{dev::Server, web, App, HttpServer};
-use sqlx::PgPool;
+use sqlx::{postgres::PgPoolOptions, PgPool};
 use tracing_actix_web::TracingLogger;
 
-use crate::email_client::EmailClient;
 use std::net::TcpListener;
 
+use crate::configuration::{DatabaseSettings, Settings};
+use crate::email_client::EmailClient;
 use crate::routes;
 
-pub fn run(
+pub struct Application {
+    port: u16,
+    server: Server,
+}
+
+impl Application {
+    pub async fn build(config: Settings) -> Result<Self, std::io::Error> {
+        let connection_pool = get_connection_pool(&config.database);
+
+        let sender_email = config
+            .email_client
+            .sender()
+            .expect("Invalid sender email address.");
+        let timeout = config.email_client.timeout();
+        let email_client = EmailClient::new(
+            config.email_client.base_url.clone(),
+            sender_email,
+            config.email_client.authorization_token.clone(),
+            timeout,
+        );
+
+        let address = format!("{}:{}", config.application.host, config.application.port);
+        let listener = TcpListener::bind(address)?;
+        let port = listener.local_addr().unwrap().port();
+        let server = run(listener, connection_pool, email_client)?;
+
+        Ok(Self { port, server })
+    }
+
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+
+    // A more expressive name that makes it clear that
+    // this function only returns when the application is stopped.
+    pub async fn run_until_stopped(self) -> Result<(), std::io::Error> {
+        self.server.await
+    }
+}
+
+pub fn get_connection_pool(config: &DatabaseSettings) -> PgPool {
+    // using connect_lazy makes sure that we won't establish a connection until the
+    // pool is used for the first time
+    PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(config.with_db())
+}
+
+fn run(
     listener: TcpListener,
     db_pool: PgPool,
     email_client: EmailClient,
